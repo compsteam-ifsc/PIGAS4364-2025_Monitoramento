3bf9526610a219fc526c53a343fabfb7ea451095
matheus <lopesmatheussouza37@gmail.com>
2026-03-05 16:14:46 -0300

    Integrando o css

--- backend/src/main/java/com/example/demo/HomeController.java
+++ backend/src/main/java/com/example/demo/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Controller
-public class Controller{
+public class HomeController{
    @GetMapping("/home")
    public String home(){
    return "index";
