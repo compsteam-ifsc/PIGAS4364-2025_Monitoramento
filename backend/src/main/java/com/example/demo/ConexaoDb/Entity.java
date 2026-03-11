a9f76416848363f23f30b7b671da52f27ceae654
matheus <lopesmatheussouza37@gmail.com>
2026-03-11 16:38:27 -0300

    Inicio do spring conectando ao db

---
+++ backend/src/main/java/com/example/demo/ConexaoDb/Entity.java
@@ -0,0 +1,5 @@
+package com.example.demo.ConexaoDb;
+
+public @interface Entity {
+
+}
--- backend/src/main/java/com/example/demo/HomeController.java
+++ backend/src/main/java/com/example/demo/HomeController.java
@@ -2,8 +2,6 @@
 package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Controller
 public class HomeController{
