0b46e1a69386d691074d417a2369d993f221d02c
matheus <lopesmatheussouza37@gmail.com>
2026-03-25 13:01:17 -0300

    SLa

--- backend/src/main/java/com/example/demo/Controllers/usuarioController.java
+++ backend/src/main/java/com/example/demo/Controllers/usuarioController.java
@@ -1,9 +1,10 @@
 package com.example.demo.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class usuarioController {
     @GetMapping("/login")
     public String usuario(){
