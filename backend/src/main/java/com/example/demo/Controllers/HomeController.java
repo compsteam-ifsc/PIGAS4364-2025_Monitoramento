22a0a91c56f2ebaae75ecf72f941adf9e1e56383
eduardo <duduheckschmitt@gmail.com>
2026-04-08 15:41:15 -0300

    pagina bonita

--- backend/src/main/java/com/example/demo/Controllers/HomeController.java
+++ backend/src/main/java/com/example/demo/Controllers/HomeController.java
@@ -1,13 +1,19 @@
 package com.example.demo.Controllers;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/Grafico")
-    public String home() {
+    public String home(Model model, Principal principal) {
+        String nomeUsuario = (principal != null) ? principal.getName() : "Usuário";
+        model.addAttribute("nomeUsuario", nomeUsuario);
+
         return "index";
     }
 }
